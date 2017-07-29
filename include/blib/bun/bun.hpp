f98f3b1b17591191f2be01d1d5044206e6c9d4a8
BrinlessLabs <noone@noone.com>
2017-07-30 02:58:36 +0530

    updateObj string generations are complete.

--- include/blib/bun/bun.hpp
+++ include/blib/bun/bun.hpp
@@ -46,14 +46,32 @@
 #define EXPAND_CLASS_TYPE_MEMBERS_createSchema_I(z, n, CLASS_ELEMS_TUP) BOOST_PP_COMMA_IF(n) blib::bun::cppTypeToDbTypeString<decltype(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)::BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(n,1), CLASS_ELEMS_TUP))>()
 #define EXPAND_CLASS_TYPE_MEMBERS_createSchema(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), EXPAND_CLASS_TYPE_MEMBERS_createSchema_I, CLASS_ELEMS_TUP)
 
+/// @brief persistObj Helper Macros
 /// @brief Changes related to INSERT INTO
 /// @brief EXPAND_CLASS_TYPE_MEMBERS_persistObj
 #define EXPAND_FIELDS_persistObj_I(z, n, ELEMS_TUP) BOOST_PP_IDENTITY(",")() BOOST_STRINGIZE(BOOST_PP_TUPLE_ELEM(n, ELEMS_TUP))
 #define EXPAND_FIELDS_persistObj(ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_TUPLE_SIZE(ELEMS_TUP), EXPAND_FIELDS_persistObj_I, ELEMS_TUP)
 
+/// @brief Expand the variables for persistObj
+#define EXPAND_VARIABLES_persistObj_I(z, n, ELEMS_TUP) BOOST_PP_COMMA_IF(n)obj->BOOST_PP_TUPLE_ELEM(n, ELEMS_TUP)
+#define EXPAND_VARIABLES_persistObj(ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_TUPLE_SIZE(ELEMS_TUP), EXPAND_VARIABLES_persistObj_I, ELEMS_TUP)
+
+/// @brief updateObj Helper Macros
+/// @brief Changes related to UPDATE table SET
+/// @brief Expand the braces for updateObj
+#define EXPAND_BRACES_updateObj_I(z, n, ELEMS_TUP) BOOST_PP_EXPR_IF(n, ",")BOOST_STRINGIZE({} = {})
+#define EXPAND_BRACES_updateObj(ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_TUPLE_SIZE(ELEMS_TUP), EXPAND_BRACES_updateObj_I, ELEMS_TUP)
+
+/// @brief Expand the variables for updateObj
+#define EXPAND_VARIABLES_updateObj_I(z, n, ELEMS_TUP) BOOST_PP_COMMA()BOOST_STRINGIZE(BOOST_PP_TUPLE_ELEM(n, ELEMS_TUP))BOOST_PP_COMMA()obj->BOOST_PP_TUPLE_ELEM(n, ELEMS_TUP)
+#define EXPAND_VARIABLES_updateObj(ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_TUPLE_SIZE(ELEMS_TUP), EXPAND_VARIABLES_updateObj_I, ELEMS_TUP)
+
 /// @brief Create n number of string
 #define Repeat_N_String_With_Precomma_I(z, n, text)  BOOST_PP_IDENTITY(",")() BOOST_STRINGIZE(text)
 #define Repeat_N_String_With_Precomma(n, text) BOOST_PP_REPEAT(n, Repeat_N_String_With_Precomma_I, text)
+
+#define BLIB_MACRO_COMMENTS_IF(a) BOOST_PP_EXPR_IF(1, a)
+
 ///////////////////////////////////////////////////////////////////////////////
 /// Helper Macros End
 ///////////////////////////////////////////////////////////////////////////////
@@ -64,7 +82,7 @@ struct BunHelper<BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)>{\
 using ClassType = BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP);\
 \
 inline static void createSchema(){\
-"@brief createSchema for creating the schema of an object";\
+BLIB_MACRO_COMMENTS_IF("@brief createSchema for creating the schema of an object");\
 static std::string const class_name = BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP));\
 static std::string const query = "CREATE TABLE IF NOT EXISTS '{}' (oid_high INTEGER PRIMARY KEY AUTOINCREMENT, oid_low INTEGER NOT NULL" \
 EXPAND_CLASS_MEMBERS_createSchema(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP )) \
@@ -74,13 +92,13 @@ l().info(sql);\
 }\
 \
 inline static void deleteSchema(){\
-"@brief deleteSchema for deleting the schema of an object";\
+BLIB_MACRO_COMMENTS_IF("@brief deleteSchema for deleting the schema of an object");\
 static std::string const class_name = BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP));\
 static std::string const sql = fmt::format("DROP TABLE '{}'", class_name);\
 }\
 \
 inline static SimpleOID persistObj( T* obj ){\
-"@brief persistObj for persisting the object";\
+BLIB_MACRO_COMMENTS_IF("@brief persistObj for persisting the object");\
 static std::string const class_name = BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP));\
 SimpleOID oid;\
 oid.populateLow();\
@@ -90,8 +108,20 @@ EXPAND_FIELDS_persistObj(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP ))\
 ") VALUES ({}" \
 Repeat_N_String_With_Precomma(BOOST_PP_TUPLE_SIZE(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP )), {})\
 ;\
-static std::string const sql = fmt::format(query, class_name, oid.low);\
+std::string const sql = fmt::format(query, class_name, oid.low,\
+EXPAND_VARIABLES_persistObj(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP ))\
+);\
 return oid;\
+}\
+inline static void updateObj( T* obj, SimpleOID const& oid ){\
+BLIB_MACRO_COMMENTS_IF("@brief updateObj for updating a persisted object");\
+static std::string const class_name = BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP));\
+static std::string const query = "UPDATE {} SET " \
+EXPAND_BRACES_updateObj(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP )) \
+" WHERE oid_low={} AND oid_high={}";\
+static std::string const sql = fmt::format(query, class_name \
+EXPAND_VARIABLES_updateObj(BOOST_PP_TUPLE_POP_FRONT( CLASS_ELEMS_TUP )) \
+,oid.low, oid.high);\
 }\
 };\
 }\
