f57ea75182b79e956f40929a9ee3616e6d367ae5
BrainlessLabs <BrainlessLabs@users.noreply.github.com>
2018-03-08 00:19:16 +0530

    Generating more type info so that tables can be created

--- include/blib/bun/bun.hpp
+++ include/blib/bun/bun.hpp
@@ -20,6 +20,8 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/algorithm/iteration/for_each.hpp>
 #include <boost/fusion/include/for_each.hpp>
+#include <boost/fusion/support/pair.hpp>
+#include <boost/fusion/include/pair.hpp>
 #include <boost/preprocessor/tuple/rem.hpp>
 #include <third_party/fmt/format.hpp>
 #include "blib/bun/PRef.hpp"
@@ -160,6 +162,14 @@ namespace blib {
 			};
 
 			/////////////////////////////////////////////////
+			/// @class IsComposite
+			/// @brief True if the element is a class/struct that can be persisted
+			/////////////////////////////////////////////////
+			template<typename T>
+			struct IsComposite : boost::mpl::bool_<false> {
+			};
+
+			/////////////////////////////////////////////////
 			/// @class IsContainer
 			/// @brief True if the variable is a container type
 			/////////////////////////////////////////////////
@@ -235,6 +245,15 @@ namespace blib {
 			};
 
 			/////////////////////////////////////////////////
+			/// @class StripQualifiersAndMakePointer
+			/// @brief String all the qualifiers like const, volatile, removes reference and adds a pointer.
+			/////////////////////////////////////////////////
+			template<typename T>
+			struct StripQualifiersAndMakePointer {
+				using type = std::add_pointer<std::remove_reference<std::remove_cv<T>::type>::type>::type;
+			};
+
+			/////////////////////////////////////////////////
 			/// @class TypeMetaData
 			/// @brief Contains meta data that will be usieful to get type 
 			///        inferences for a certain type.
@@ -273,17 +292,17 @@ namespace blib {
 			template<>
 			struct SqlString<int> {
 				static std::string const& createSchema() {
-					static const std::string sql = "CREATE TABLE {} IF NOT EXISTS (oid_high INTEGER PRIMARY KEY AUTOINCREMENT, oid_low INTEGER NOT NULL, {} INTEGER)";
+					static const std::string sql = "CREATE TABLE '{}' IF NOT EXISTS (oid_high INTEGER PRIMARY KEY AUTOINCREMENT, oid_low INTEGER NOT NULL, {} INTEGER)";
 					return sql;
 				}
 				
 				static std::string const& deleteSchema() {
-					static const std::string sql = "DROP TABLE {}";
+					static const std::string sql = "DROP TABLE '{}'";
 					return sql;
 				}
 				
 				static std::string const& updateObject() {
-					static const std::string sql = "UPDATE {} SET {} = {}";
+					static const std::string sql = "UPDATE '{}' SET {} = {}";
 					return sql;
 				}
 				
@@ -306,12 +325,29 @@ namespace blib {
 				static std::string _update_table_sql;
 				static std::string _table_name;
 
+				struct CreateTable {
+					std::string& sql;
+
+					CreateTable(std::string& sql) : sql(sql) {}
+
+					template <typename T>
+					void operator()(T const& x) const
+					{
+					}
+				};
+
 				inline static void createSchema() {
 					_table_name = TypeMetaData<T>::class_name();
 				}
 
-				inline static void createSchema(const std::string parent_table, const std::string table) {
+				inline static void createSchema(const std::string table, const std::string parent_table) {
 
+					if (_create_table_sql.empty()) {
+						_create_table_sql = "CREATE TABLE '{}' IF NOT EXISTS (oid_high INTEGER PRIMARY KEY AUTOINCREMENT, oid_low INTEGER NOT NULL";
+						TypeMetaData<T>::TupType tup = {nullptr};
+						boost::fusion::filter_view<T, IsComposite<boost::mpl::_>> composites_view(tup);
+						_create_table_sql += ")"
+					}
 				}
 
 				inline static void deleteSchema();
@@ -360,8 +396,14 @@ std::is_fundamental<BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)::BOOST_PP_TUPLE_ELEM
 },
 #define EXPAND_MEMBER_ASSIGNENTS_generate_type_maps(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), EXPAND_MEMBER_ASSIGNENTS_generate_type_maps_I, CLASS_ELEMS_TUP)
 
-#define GENERATE_TUPLE_OF_TYPES_PTR_I(z, n, CLASS_ELEMS_TUP) BOOST_PP_IF(n, BOOST_PP_COMMA, BOOST_PP_EMPTY)() std::add_pointer<std::remove_reference<std::remove_cv<BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)::BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(1, n), CLASS_ELEMS_TUP)>::type>::type>::type
-#define GENERATE_TUPLE_OF_TYPES_PTR(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), GENERATE_TUPLE_OF_TYPES_PTR_I, CLASS_ELEMS_TUP)
+#define GENERATE_TupType_I(z, n, CLASS_ELEMS_TUP) BOOST_PP_IF(n, BOOST_PP_COMMA, BOOST_PP_EMPTY)() std::add_pointer<std::remove_reference<std::remove_cv<decltype(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP) :: BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(1, n), CLASS_ELEMS_TUP))>::type>::type>::type
+#define GENERATE_TupType(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), GENERATE_TupType_I, CLASS_ELEMS_TUP)
+
+#define GENERATE_TupTypePair_I(z, n, CLASS_ELEMS_TUP) BOOST_PP_IF(n, BOOST_PP_COMMA, BOOST_PP_EMPTY)() boost::fusion::pair<blib::bun::__private::StripQualifiersAndMakePointer<decltype(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP) :: BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(1, n), CLASS_ELEMS_TUP))>::type, std::string>
+#define GENERATE_TupTypePair(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), GENERATE_TupTypePair_I, CLASS_ELEMS_TUP)
+
+#define GENERATE_TupTypePairObj_I(z, n, CLASS_ELEMS_TUP) BOOST_PP_IF(n, BOOST_PP_COMMA, BOOST_PP_EMPTY)() boost::fusion::make_pair<blib::bun::__private::StripQualifiersAndMakePointer<decltype(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP) :: BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(1, n), CLASS_ELEMS_TUP))>::type>(BOOST_STRINGIZE(BOOST_PP_TUPLE_ELEM(BOOST_PP_ADD(1, n), CLASS_ELEMS_TUP)))
+#define GENERATE_TupTypePairObj(CLASS_ELEMS_TUP) BOOST_PP_REPEAT(BOOST_PP_SUB(BOOST_PP_TUPLE_SIZE(CLASS_ELEMS_TUP), 1), GENERATE_TupTypePairObj_I, CLASS_ELEMS_TUP)
 ///////////////////////////////////////////////////////////////////////////////
 /// Helper Macros End
 ///////////////////////////////////////////////////////////////////////////////
@@ -383,9 +425,17 @@ static const DbTypes ret = DbTypes::kComposite;\
 }\
 namespace blib{namespace bun{namespace __private{\
 template<>\
+struct IsComposite<BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)> : boost::mpl::bool_<true> {\
+};\
+template<>\
 TypeMetaData<BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP)>{\
-using TupType = boost::fusion::tuple<GENERATE_TUPLE_OF_TYPES_PTR(CLASS_ELEMS_TUP)>;\
-using T=BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP);\
+using TupType = boost::fusion::tuple<GENERATE_TupType(CLASS_ELEMS_TUP)>;\
+using TupTypePairType = boost::fusion::tuple<GENERATE_TupTypePair(CLASS_ELEMS_TUP)>;\
+using T = BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP);\
+static auto tuple_type_pair()->TupTypePairType const&{\
+static const TupTypePairType t{GENERATE_TupTypePairObj(CLASS_ELEMS_TUP)};\
+return t;\
+}\
 static std::string const& class_name(){\
 static std::string const class_name = BOOST_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, CLASS_ELEMS_TUP));\
 return class_name;\
