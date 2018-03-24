a8dbcb314d07e46e6071ffb99d221edd4d481736
BrainlessLabs <BrainlessLabs@users.noreply.github.com>
2018-03-24 14:45:42 +0530

    Some more changes to fix the query
    Fixing exceptions in the SQL.
    
    Co-Authored-By: BrainlessLabs <sp-mishra@users.noreply.github.com>

--- src/Generate.cpp
+++ src/Generate.cpp
@@ -12,7 +12,7 @@
 using namespace soci;
 using namespace std;
 
-namespace backery {
+namespace bakery {
 	struct A {
 		int i;
 		A() = default;
@@ -30,45 +30,64 @@ namespace backery {
 	};
 }
 
-SPECIALIZE_BUN_HELPER((backery::A, i));
+SPECIALIZE_BUN_HELPER((bakery::A, i));
 
-namespace backery {
+namespace bakery {
 	struct Bun {
 		std::string bun_name;
 		double sugar_quantity;
 		int bun_length;
+		std::string json;
 		int a;
+		Bun() :bun_name(), sugar_quantity(123.12), bun_length(77), a() {}
 	};
 }
 
-SPECIALIZE_BUN_HELPER((backery::Bun, bun_name, sugar_quantity, bun_length, a));
+SPECIALIZE_BUN_HELPER((bakery::Bun, bun_name, sugar_quantity, bun_length, json, a));
 
 int main() {
 	namespace bun = blib::bun;
 	namespace query = blib::bun::query;
 
-	auto str = blib::bun::__private::SqlString<backery::Bun>::create_table_sql();
-	str = blib::bun::__private::SqlString<backery::Bun>::insert_row_sql();
+	auto str = blib::bun::__private::SqlString<bakery::Bun>::create_table_sql();
+	str = blib::bun::__private::SqlString<bakery::Bun>::insert_row_sql();
 
 	bun::connect("postgresql://localhost/postgres?user=postgres&password=postgres");
 	//blib::bun::connect("objects.db");
-	blib::bun::createSchema<backery::Bun>();
+	blib::bun::createSchema<bakery::Bun>();
 
-	blib::bun::PRef<backery::Bun> bunn = new backery::Bun;
+	blib::bun::PRef<bakery::Bun> bunn = new bakery::Bun;
 	const auto oid = bunn.save();
 	bunn->bun_length = 11;
 	bunn->bun_name = "test";
 	bunn->sugar_quantity = 55.6;
 	bunn.save();
-	blib::bun::SimpleOID oid1(1, 24134930076892);
-	blib::bun::PRef<backery::Bun> bun1(oid1);
+	blib::bun::SimpleOID oid1(1, 5582309293008);
+	blib::bun::PRef<bakery::Bun> bun1(oid1);
 	bun1.del();
-	for (int i = 0; i < 1000; ++i) {
-		blib::bun::PRef<backery::Bun> bunn = new backery::Bun;
+	
+	for (int i = 0; i < 5; ++i) {
+		blib::bun::PRef<bakery::Bun> bunn = new bakery::Bun;
+		bunn->bun_name = i % 2 ? "Delete Me" : "Do not Delete Me";
 		bunn->bun_length = i;
 		bunn->sugar_quantity = 55.6 * i;
-		bunn->bun_name = fmt::format("{}", bunn.toJson());
+		bunn->json = "";
+		bunn->json = fmt::format("{}", bunn.toJson());
 		const auto oid = bunn.save();
 	}
+
+	using BunFields = query::F<bakery::Bun>;
+	using FromBun = query::From<bakery::Bun>;
+	FromBun fromBun;
+
+	auto valid_query = BunFields::bun_length > 2 && BunFields::bun_name == "Delete Me";
+	std::cout << "Press any key to delete" << std::endl;
+	char c;
+	std::cin >> c;
+	auto buns = fromBun.where(valid_query).objects();
+	for (auto bun : buns) {
+		bun.del();
+	}
+	
 	return 1;
 }
