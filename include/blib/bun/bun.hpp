a63396f1f8da0420eb7e2a481c50147050ecabf9
BrainlessLabs <BrainlessLabs@users.noreply.github.com>
2018-03-27 23:35:15 +0530

    Fixing the insert
    Co-Authored-By: BrainlessLabs <sp-mishra@users.noreply.github.com>

--- include/blib/bun/bun.hpp
+++ include/blib/bun/bun.hpp
@@ -1384,7 +1384,8 @@ namespace blib {
 						const std::string& parent_column_name = obj_name;
                         QueryHelper<O>::deleteObjWithParentInfo(oid_ref, parent_table_reference, parent_column_name);
                         const blib::bun::SimpleOID oid = QueryHelper<O>::persistObj(&x, oid_ref, parent_table_reference, parent_column_name);
-						val.set<typename ConvertCPPTypeToSOCISupportType<blib::bun::SimpleOID::OidHighType>::type>(obj_name, oid.high, ind);
+						const std::string oids = "{" + fmt::format("'oid_high': {}, 'oid_low': {}", oid.high, oid.low) + "}";
+						val.set<typename ConvertCPPTypeToSOCISupportType<std::string>::type>(obj_name, oids, ind);
 					}
 				};
 
